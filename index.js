698b746c9510b31f5a6060655d2d4ac07240cd55
Saba Haddis <sabahaddissolomon@gmail.com>
2026-07-03 03:28:16 +0300

    feat: add domain model interfaces & integration tests

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+import { Temporal } from "@js-temporal/polyfill";
+const student = {
+    id: "STU-001",
+    name: "Hana Tadesse",
+    enrollmentDate: Temporal.Now.instant(),
+};
+//Try these what does the compiler say?
+student.id = "STU-999";
+console.log(student.gpa.toFixed(2));
+console.log(student.gpa?.toFixed(2) ?? "Not yet graded");
 //# sourceMappingURL=index.js.map
---
+++ models/student.model.js
@@ -0,0 +1,2 @@
+import { Temporal } from "@js-temporal/polyfill";
+//# sourceMappingURL=student.model.js.map
